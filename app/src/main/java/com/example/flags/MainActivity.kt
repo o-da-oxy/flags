085c1c5e8a3b90c8f513dafc88344c9cec48f831
Oksana <demskaya01@gmail.com>
2021-06-16 02:17:19 +0300

    makeInteractive(), override onBackPressed()

--- app/src/main/java/com/example/flags/MainActivity.kt
+++ app/src/main/java/com/example/flags/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.flags
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        makeInteractive()
+    }
+
+    fun makeInteractive() {
+        //получаем layout со всеми флагами
+        val flags = findViewById<ViewGroup>(R.id.flags)
+        //сделать для каждого флага обработчик нажатия
+        for (i in 0 until flags.childCount) {
+            val v = flags.getChildAt(i)
+            v.setOnClickListener() {
+                flags.removeView(it)
+                it.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+                setContentView(it)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        setContentView(R.layout.activity_main)
+        makeInteractive()
     }
 }
